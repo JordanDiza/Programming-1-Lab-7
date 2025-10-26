f2599538dd5176a2666ee3fce5138f841a3b060d
JordanDiza <dizaj8@gmail.com>
2025-10-26 12:31:24 -0400

    36

--- Automaton.java
+++ Automaton.java
@@ -53,11 +53,14 @@ public class Automaton
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
-            int right = (i + 1 < state.length) ? state[i + 1] : 0;
-            
-            nextState[i] = (left + center + right) % 2;
+            int right = state[i + 1];
+            nextState[i] = calculateNextState(left, center, right);
             left = center;
             center = right;
+        }
+        for (int i = 0; i < numberOfCells; i++) 
+        { 
+        state[i] = nextState[i];
         }
         state = nextState;
     }

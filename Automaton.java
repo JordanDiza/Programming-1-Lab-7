926400e5e3c2d0cdf8d8daedea227af43a4a9bc3
JordanDiza <dizaj8@gmail.com>
2025-10-26 12:08:42 -0400

    29

--- Automaton.java
+++ Automaton.java
@@ -78,5 +78,7 @@ public class Automaton
         Arrays.fill(state, 0);
         // Seed the automaton with a single 'on' cell.
         state[numberOfCells / 2] = 1;
+        state[numberOfCells / 2-1] = 1;
+        state[numberOfCells / 2 + 1] = 1;
     }
 }
